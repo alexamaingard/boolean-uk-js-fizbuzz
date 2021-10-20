e417f636313d5b839c906e7186c1b01a7e280b9b
lexymcr <@maingard.alexa@gmail.com>
2021-10-20 16:33:29 +0200

    Loop Correction

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const divBy3 = "Fizz";
 const divBy5 = "Buzz";
 const divBy7 = "Fish";
 const divBy8 = "Bus";
+let divByAll;
 
 for (let i = 1; i < 102; i++){
-    if(i > 100) {
-        let j;
-        for (j = 101; ; j++) {
-            if(j%3 == 0 && j%5 == 0 && j%7 == 0 && j%8 == 0) {
-                break;
-            }
-        }
-        console.log(j);
-    }
-    else if (i%3 == 0 && i%5 == 0){
+    if (i%3 == 0 && i%5 == 0){
         console.log(divBy3 + divBy5);
     }
     else if (i%3 == 0) {
@@ -32,3 +24,10 @@ for (let i = 1; i < 102; i++){
         console.log(i);
     }
 }
+for (let j = 101; ; j++) {
+    if(j%3 == 0 && j%5 == 0 && j%7 == 0 && j%8 == 0) {
+        divByAll = j;
+        break;
+    }
+}
+console.log(divByAll);
